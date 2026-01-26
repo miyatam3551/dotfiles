85bda8378535922539868c42e39efa3bb3d9070c
miyatam3551 <miyatam3551@gmail.com>
2026-01-26 20:40:44 +0900

    🔧 タブが1つでもステータスラインを常に表示
    tabline.wez の情報を常に確認できるようにするため
    
    - 🔄 hide_tab_bar_if_only_one_tab を false に変更

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -68,7 +68,7 @@ config.window_background_image_hsb = {
 
 -- タブバー設定
 config.enable_tab_bar = true
-config.hide_tab_bar_if_only_one_tab = true
+config.hide_tab_bar_if_only_one_tab = false  -- タブが1つでもステータスラインを表示
 config.use_fancy_tab_bar = false  -- tabline.wez と互換性を保つため false に
 config.tab_bar_at_bottom = true   -- タブバーを下部に配置
 -- スクロールバック
