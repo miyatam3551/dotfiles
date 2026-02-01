f509982354234e6eb3b2f4d914ea35597599ab17
miyatam3551 <miyatam3551@gmail.com>
2026-02-01 10:34:26 +0900

    nvim: marksman LSP を無効化
    Obsidianノートで大量のWikiリンクエラーが表示される問題を解消

--- dot_config/nvim/lua/plugins/markdown.lua
+++ dot_config/nvim/lua/plugins/markdown.lua
@@ -1,6 +1,18 @@
 -- lang.markdown の機能を無効化
 -- Obsidian ノートにはリンターもレンダリングも不要
 return {
+  -- marksman LSP を無効化（Wikiリンクの検証でエラーが大量発生するため）
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        marksman = {
+          enabled = false,
+        },
+      },
+    },
+  },
+
   -- markdownlint を無効化
   {
     "mfussenegger/nvim-lint",
