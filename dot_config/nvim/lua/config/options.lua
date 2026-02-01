c94fb89859e679d5df749b16c7f14e7c3c2c65ba
miyatam3551 <miyatam3551@gmail.com>
2026-02-01 10:23:28 +0900

    fix: 相対行番号を無効化

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- 相対行番号を無効化（通常の行番号のみ表示）
+vim.opt.relativenumber = false
